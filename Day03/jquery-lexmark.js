e82fd076b68fbc33878f9166b14debd2711131f7
Prabhu Sunderaraman <iamprabhu@yahoo.com>
2013-10-24 15:06:14 +0530

    Day 03

--- Day03/jquery-lexmark.js
+++ Day03/jquery-lexmark.js
@@ -1,3 +1,15 @@
-﻿$.fn.bgcolor = function (value) {
+﻿$.fn.underline = function () {
+    $(this).css("text-decoration", "underline");
+    return $(this);
+}
+$.fn.bgcolor = function (value) {
     $(this).css("background-color", value);
+}
+$.fn.toCaps = function () {
+    $(this).css("text-transform", "uppercase");
+    //$(this).html($(this).html().toUpperCase());
+}
+$.fn.bold = function () {
+    $(this).css("font-weight", "bold");
+    return $(this);
 }
